b2c822ff19127640227241b00b1913303db56fa6
Leticia <leal.b@aluno.ifsp.edu.br>
2023-03-21 16:56:23 -0300

    exercicioAquario

---
+++ src/semana05/exercicioAquario.java
@@ -0,0 +1,38 @@
+package semana05;
+import java.util.Scanner;
+
+public class exercicioAquario {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Qual o comprimento do aquário: ");
+        int c = sc.nextInt();
+
+        Scanner sal = new Scanner(System.in);
+        System.out.println("Qual a altura do aquário: ");
+        int a = sc.nextInt();
+
+        Scanner sl = new Scanner(System.in);
+        System.out.println("Qual a largura do aquário: ");
+        int l = sc.nextInt();
+
+        Scanner sd = new Scanner(System.in);
+        System.out.println("Qual a temperatura desejada do aquário: ");
+        int td = sc.nextInt();
+
+        Scanner sa = new Scanner(System.in);
+        System.out.println("Qual a temperatura ambiente do aquário: ");
+        int ta = sc.nextInt();
+        
+        double v = (c * a * l) / 1000;
+        System.out.println("O volume do aquario é:" + v);
+
+        double p = v * 0.05 * (td - ta);
+        System.out.println("A potência do termostato é:" + p);
+
+        double f = v * 2;
+        System.out.println("A quantidade de filtragem é:" + f);
+    }
+
+}
---
+++ src/semana05/exercicioIMC.java
@@ -0,0 +1,14 @@
+package semana05;
+
+public class IMC {
+    public static void main(String[] args) {
+        
+        int idade = 17;
+        final int MAIORIDADE = 18;
+
+        if (idade >= MAIORIDADE) {
+            System.out.println("Maior de idade");
+        }
+
+
+        
